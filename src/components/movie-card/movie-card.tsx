362539934f289a8b6e3ddc04f304e58ad938b1b4
A U M Tuhin <a.tuhin@greyrook.com>
2022-08-03 20:11:32 +0200

    removed html tags from movie summary with regex

--- src/components/movie-card/movie-card.tsx
+++ src/components/movie-card/movie-card.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './movie-card.styles.scss';
 
 const MovieCard: React.FC<{ movie: any }> = ({ movie }) => {
+    // to remove the html tags from summary, api serves html template rather than plain text as movie summary
+    const regex = /(<([^>]+)>)/ig;
     return (
         <div className='movie-card'>
             {movie.name && <h4 className='title'>{movie.name}</h4>}
             {movie.image !== null && <img className='movie-thumb' src={movie.image.medium} alt="thumb" />}
             {movie.rating.average !== null && <div className='rating'>Rating: {movie.rating.average}</div>}
             <div>Language: {movie.language}</div>
-            {movie.summary !== null && <p className='summary'>{movie.summary}</p>}
+            {movie.summary !== null && <p className='summary'>{movie.summary.replace(regex, "")} ...</p>}
         </div>
     )
 }
