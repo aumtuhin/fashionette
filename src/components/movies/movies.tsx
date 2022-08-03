0774a2c6d2db7952f735ddce24c195da09b4a03a
A U M Tuhin <a.tuhin@greyrook.com>
2022-08-03 20:56:34 +0200

    showed total numbers in movies and no result message

--- src/components/movies/movies.tsx
+++ src/components/movies/movies.tsx
@@ -3,17 +3,20 @@ import { useContext } from 'react';
 import MovieCard from '../movie-card/movie-card';
 import { MovieContext } from '../../contexts/movieContext';
 import './movies.styles.scss';
+import { Movie } from '../../shared/interfaces';
 
 const Movies = () => {
     const { movies, isLoading } = useContext(MovieContext);
 
     return (
         <div className='movies-section'>
+            <div>Total: {movies.length}</div>
             <div className='movies-container'>
                 {isLoading && <div>Loading...</div>}
-                {!isLoading && movies.length > 0 && movies.map((movie: any, index: number) => (
+                {!isLoading && movies.length > 0 && movies.map((movie: Movie, index: number) => (
                     <MovieCard key={index} movie={movie} />
                 ))}
+                {!isLoading && movies.length === 0 && <div>Oops!! Please try with different name</div>}
             </div>
         </div>
     )
